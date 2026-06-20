2c49dc46f4601f9aaf389ae3b112989507e9de76
Liarus_ <45693898+GuardianLiarus@users.noreply.github.com>
2026-06-20 10:33:55 +0200

    Update and rename setup.sh to wayland_setup.sh

--- wayland_setup.sh
+++ wayland_setup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# Rotate gyroscope sensor
+# Rotate the accelerometer sensor
 echo $'ENV{ACCEL_MOUNT_MATRIX}="0, -1, 0; -1, 0, 0; 0, 0, 1"' | sudo tee \
     /etc/udev/rules.d/99-gole2pro-accel.rules
 
-#Rotate touch input by 180 degrees, r
+#Rotate touch input by 180 degrees
 echo $'ENV{LIBINPUT_CALIBRATION_MATRIX}="-1 0 1 0 -1 1"' | sudo tee \
     /etc/udev/rules.d/99-gole2pro-touch.rules
 sudo reboot
